1e7efa2636fc34f879b92dbfec6a67247ab4c3e3
Maaz Sirkhot <maazsirkhot0@gmail.com>
2020-03-28 14:12:23 -0700

    Configuration changes for deployment

---
+++ src/test/java/com/cmpe275lab2/test/Cmpe275Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpe275lab2.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpe275Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
